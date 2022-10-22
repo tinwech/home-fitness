c3c8a5d4bc23b702830ed8ed705dca183e4a2594
tinwech <tinwech91@gmail.com>
2022-10-22 10:38:54 +0800

    update comparing algorithm

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from distutils.log import error
 from flask import Flask, render_template, Response
 import numpy as np
 import cv2 as cv
@@ -8,28 +9,70 @@ f = cv.FileStorage('calibrate.xml', cv.FILE_STORAGE_READ)
 intrinsic = f.getNode('intrinsic').mat()
 distortion = f.getNode('distortion').mat()
 app = Flask(__name__)
+t_errors = []
+r_errors = []
 
+def unit_vector(vector):
+    return vector / np.linalg.norm(vector)
+
+def angle_between(v1, v2):
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
 def pose_matching(demo, user):
+    t_error = 0
+    r_error = 0
+
+    # no markers detected in demo video
     if demo[0][0] is None:
         return
 
+    # no markers detected in user video
+    if user[0][0] is None:
+        t_errors.append(50)
+        r_errors.append(3)
+        return
+
+    # demo[0] = ids, demo[1] = tvec[], demo[2] = rvec[]
     demo = np.array(demo)
     demo = demo[:, demo[0, :].argsort()]
     user = np.array(user)
     user = user[:, user[0, :].argsort()]
     
-    # print(user)
-    # for i in range(len(ids)):
-    #     print(ids[i]) 
-    #     print(tvec[i])
-    #     print(rvec[i])
+    j = 0
+    for i in range(len(demo[0])):
+        while j < len(user[0])  and demo[0][i] < user[0][j]:
+            j += 1
+        
+        # can't find demo marker id[i] in user marker ids
+        if j >= len(user[0]) or demo[0][i] != user[0][j]:
+            break
+
+        target_tvec = demo[1][i]
+        target_rvec = demo[2][i]
+        user_tvec = user[1][j]
+        user_rvec = user[2][j]
+
+        # translation error
+        d = user_tvec - target_tvec
+        t_error += np.sqrt(np.sum(np.square(d)))
+        
+        # rotation error
+        theta = angle_between(user_rvec, target_rvec)
+        r_error += theta
+        e = np.linalg.norm(user_rvec) - np.linalg.norm(target_rvec)
+        r_error += e
+
+    print(f'translation error: {t_error}, rotation error: {r_error}')
+    t_errors.append(t_error)
+    r_errors.append(r_error)
+
     return
 
 def pose_esitmation(frame):
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # cv.aruco_dict = cv.aruco.Dictionary_get(cv.aruco.DICT_5X5_50)
     cv.aruco_dict = cv.aruco.Dictionary_get(cv.aruco.DICT_ARUCO_ORIGINAL)
     parameters = cv.aruco.DetectorParameters_create()
 
@@ -70,8 +113,10 @@ def gen_frames():
     while True:
         ret, frame_demo = demo.read()  # frame (480, 640, 3)
 
+        # demo video ended
         if not ret:
-            demo = cv.VideoCapture('./demos/demo_1.avi')
+            demo = cv.VideoCapture('./demos/demo.avi')
+            # evaluation()
             continue
 
         ret, frame_user = user.read()

edfd1a788b7b01cb795632ed904913c60c326a23
tinwech <tinwech91@gmail.com>
2022-10-22 09:23:06 +0800

    edit demo file name

--- demo.py
+++ demo.py
@@ -3,7 +3,7 @@ import cv2
 cap = cv2.VideoCapture(1)
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-i = input()
+action = input()
 demo = False
 while cap.isOpened():
     ret, frame = cap.read()
@@ -19,7 +19,7 @@ while cap.isOpened():
     cv2.imshow('frame', frame)
     if cv2.waitKey(30) == ord('d'):
         demo = True 
-        out = cv2.VideoWriter(f'./demos/demo_{i}.avi', fourcc, 20.0, (640,  480))
+        out = cv2.VideoWriter(f'./demos/{action}_demo.avi', fourcc, 20.0, (640,  480))
     elif cv2.waitKey(30) == ord('q'):
         break
 # Release everything if job is finished
